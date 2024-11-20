efe2eb1254ef94bc2dca3a0ccb9601bd06f1a836
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2024-11-21 00:53:57 +0530

    feat : add header comp

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='app' >
+        <div className='app-header'>
+          <span>LOGO</span>
+          <h2>QUIZ</h2>
+        </div>
+        <div className='progress' >
+
+        </div>
+    </div>
+  )
+} 
+
+export default App
+
+https://opentdb.com/api.php?amount=15&category=23&difficulty=medium&type=multiple
